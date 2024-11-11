35f27427be5d5605da9fc9a9193b91b0e8a6f7d9
unknown <rubleeopt@gmail.com>
2024-11-12 00:06:48 +0800

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'y.qq.com',
+          pathname: '/music/photo_new/**',
+        },
+      ],
+    },
+  }
+  
+  module.exports = nextConfig 
